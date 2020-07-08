ca2ec92361a95908df8c54c29d7eea0271074739
WojTechM <wojciech.m197@gmail.com>
2020-07-08 18:12:39 +0200

    Poprawka asercji - wyrzucanie błędu a nie wyjątku

--- WUnit/src/main/java/com/github/wojtech/wunit/Assertions.java
+++ WUnit/src/main/java/com/github/wojtech/wunit/Assertions.java
@@ -4,15 +4,9 @@ import java.util.Objects;
 
 public class Assertions {
 
-    public static class AssertionException extends RuntimeException {
-        public AssertionException(String message) {
-            super(message);
-        }
-    }
-
     public static void assertEquals(Object o1, Object o2){
         if (!Objects.equals(o1, o2)) {
-            throw new AssertionException(String.format("Objects %s and %s are not equal", o1, o2));
+            throw new AssertionError(String.format("Objects %s and %s are not equal", o1, o2));
         }
     }
 }
